a4460bb2d74a9ba9fbe1641856494ade5609e350
ByteMaster <135186285+ByteMaster-Y@users.noreply.github.com>
2024-01-17 04:18:50 +0900

    Time: 0 ms (100.00%), Space: 8.7 MB (69.50%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -5,20 +5,18 @@ public:
     bool check(std::vector<int>& nums) {
         int n = nums.size();
         int rotated_count = 0;
-
-        // Count the number of rotations
-        for (int i = 0; i < n - 1; ++i) {
-            if (nums[i] > nums[i + 1]) {
+        
+        for (int i = 0; i < n - 1; ++i){
+            if (nums[i] > nums[i+1]){
                 rotated_count++;
             }
         }
-
-        // Handle the circular property
-        if (nums[n - 1] > nums[0]) {
+        
+         
+        if (nums[n - 1] > nums[0]){
             rotated_count++;
         }
 
-        // There should be at most one rotation for a sorted and rotated array
-        return rotated_count <= 1;
+        return rotated_count<=1;
     }
 };
